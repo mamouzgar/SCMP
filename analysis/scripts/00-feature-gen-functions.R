c4ae107615e8a0e49d8c2930c7eed63c724d77ee
Meelad Amouzgar <mamouzgar@DNa80e9de.SUNet>
2020-09-18 22:50:44 -0700

    changed feature generation script to save models

--- analysis/scripts/00-feature-gen-functions.R
+++ analysis/scripts/00-feature-gen-functions.R
@@ -21,43 +21,51 @@ set.seed(0)
 ## PCA
 ## data is centered and scaled
 pca_function <- function(input.data) { 
-  pca.data <- prcomp(dplyr::select(input.data, -gate), center = TRUE, scale = TRUE)
-  pca.data <- data.frame(pca.data$x) %>%
+  pca.model <- prcomp(dplyr::select(input.data, -gate), center = TRUE, scale = TRUE)
+  pca.data <- data.frame(pca.model$x) %>%
     dplyr::mutate(gate = input.data[ , "gate"])
   
   write.table(pca.data, paste0(output_filename, "pca.csv"),sep = ",", row.names = FALSE, col.names = TRUE)
+  saveRDS(pca.model, paste0(output_filename, "pca.RDS"))
+  
   return(pca.data)
 }
 
 
 ## tsne (with PCA)
 ## note: uses Rtsne's internal PCA, generates 2-dimensions
-## perplexity is defaulted to 5% of data size if arguement is not provided
+## perplexity is defaulted to 25 if arguement is not provided
+## potential future added functionality: perplexity is defaulted to 5% of data size if arguement is not provided
 ## other features set to default 
 tsne_function <- function(input.data, perplexity.value = NULL)  { 
   if(is.null(perplexity.value)) { 
-    perplexity.value  <- nrow(input.data)*0.05
+    # perplexity.value  <- nrow(input.data)*0.05
+    perplexity.value  <- 25
   }
   
-  tsne.data <- Rtsne(dplyr::select(input.data, -gate), dims=2, 
+  tsne.model <- Rtsne(dplyr::select(input.data, -gate), dims=2, 
                      perplexity= perplexity.value , 
                      pca = TRUE, pca_center = TRUE, pca_scale = TRUE, normalize = TRUE, 
                      theta=0.5,  max_iter = 1000, check_duplicates = FALSE)
-  tsne.data <- data.frame(tsne.data$Y) %>%
+  tsne.data <- data.frame(tsne.model$Y) %>%
     dplyr::mutate(gate = input.data[ , "gate"])
   
   write.table(tsne.data, paste0(output_filename, "tsne.csv"),sep = ",", row.names = FALSE, col.names = TRUE)
+  saveRDS(tsne.model, paste0(output_filename, "tsne.RDS"))
+  
   return(tsne.data)
 }
 
 
 ## UMAP
 umap_function <- function(input.data)  {
-  umap.data <- umap(dplyr::select(input.data, -gate))
-  umap.data <- data.frame(umap.data$layout) %>%
+  umap.model <- umap(dplyr::select(input.data, -gate))
+  umap.data <- data.frame(umap.model$layout) %>%
     dplyr::mutate(gate = input.data[ , "gate"])
   
   write.table(umap.data, paste0(output_filename, "umap.csv"),sep = ",", row.names = FALSE, col.names = TRUE)
+  saveRDS(umap.model, paste0(output_filename, "umap.RDS"))
+  
   return(umap.data)
 }
 
@@ -83,11 +91,12 @@ phate_function <-  function(input.data, knn.value = NULL, decay.value = NULL, ga
   } 
   
   
-  phate.data <- phate(dplyr::select(input.data, -gate), knn = knn.value, decay = decay.value, )
-  # phate.data <- data.frame(phate.data$layout) %>%
-  #   dplyr::mutate(gate = input.data[ , "gate"])
+  phate.model <- phate(dplyr::select(input.data, -gate), knn = knn.value, decay = decay.value, )
+  phate.data <- data.frame(phate.model$embedding) %>%
+    dplyr::mutate(gate = input.data[ , "gate"])
   
   write.table(phate.data, paste0(output_filename, "phate.csv"),sep = ",", row.names = FALSE, col.names = TRUE)
+  saveRDS(phate.model, paste0(output_filename, "phate.RDS"))
   
   return(phate.data)
 }
@@ -103,11 +112,13 @@ phate_function <-  function(input.data, knn.value = NULL, decay.value = NULL, ga
 #   
 #   dists.data <- dist(dplyr::select(input.data, -gate),) ## compute pairwise distance matrix
 #   
-#   dmap.data <- diffuse(dists.data)
-#   dmap.data <- data.frame(dmap.data$layout) %>%
+#   dmap.model <- diffuse(dists.data)
+#   dmap.data <- data.frame(dmap.model$layout) %>%
 #     dplyr::mutate(gate = input.data[ , "gate"])
 #   
 #   write.table(dmap.data, paste0(output_filename, "umap.csv"),sep = ",", row.names = FALSE, col.names = TRUE)
+# saveRDS(dmap.model, paste0(output_filename, "dmap.RDS"))
+
 #   return(dmap.data)
 # }
 
--- analysis/scripts/01-generate-feaures.R
+++ analysis/scripts/01-generate-feaures.R
@@ -12,6 +12,8 @@ library(tidyverse)
 library(factoextra)
 library(Rtsne)
 library(umap)
+
+reticulate::use_python("/Users/mamouzgar/opt/anaconda3/bin/python")
 library(phateR)
 library(diffusionMap)
 set.seed(0)
@@ -31,22 +33,25 @@ source("SCMP/analysis/scripts/00-feature-gen-functions.R")
 ## GLOBAL VARIABLES ##
 ######################
 dat <- data.table::fread("/Users/mamouzgar/phd-projects/SCMP/data/analysis-ready/processed_LDaxes.csv", sep = ",", stringsAsFactors = FALSE)
+dat[ , "cell.id"] <- 1:nrow(dat)
 
 output_filepath <- "/Users/mamouzgar/phd-projects/SCMP/data/analysis-ready/" ## output directory 
-subset_number <- 100000 ## # of cells (rows) to include in analysis
+subset_number <- 100 ## # of cells (rows) to include in analysis
 
 ## relevant features in analysis 
 # CD.Ig.markers <- c("IgL", "IgK", "CD235ab", "CD71", "CD61", "CD3", "CD8", "CD2", "CD5", "CD4", "CD7", "CD11c", "CD23", "CD123", "CD56", "CD45", "CD10", "CD13", "CD117", "CD34", "CD20", "CD19", "CD22", "CD79a", "CD15", "CD33", "CD14", "CD64", "CD16" , "CD38" )
-CD.Ig.markers <- colnames(dat)[grepl("CD|Ig", colnames(dat))]
+CD.Ig.markers <- colnames(dat)[grepl("CD|Ig|HLA", colnames(dat))]
 scatterbodies <- c("MPO","WGA_102", "WGA_104", "WGA_105", "WGA_106", "WGA_108", "WGA_110", "barium", "lactoferrin", "rRNA", "HP1b", "lamin_B", "VAMP_7", "lysozyme", "serpin_B1", "lamin_A_C", "beta_actin" )
 all.markers <- c(CD.Ig.markers, scatterbodies)
 
 dat.clean <- dat %>%
   dplyr::filter(gate != "ungated") %>%
-  sample_n(subset_number) %>%
-  dplyr::select(-Time, -Event_length, -Bead_1, -DNA_1, -DNA_2, -Viability, -ld1,-ld2,-beadDist,-file) ## remove irrelevant markers - confirm with David
+  dplyr::sample_n(subset_number)  %>%
+  tibble::column_to_rownames("cell.id") %>%
+  dplyr::select( -Time, -Event_length, -Bead_1, -DNA_1, -DNA_2, -Viability, -ld1,-ld2,-beadDist,-file) ## remove irrelevant markers - confirm with David
 
-
+## write cell.ids used in this analysis
+write.table(rownames(dat.clean), paste0(output_filepath, Sys.Date(), "_analyzed-cells_", subset_number, "-cells.csv"), sep=",",row.names = FALSE, col.names = TRUE)
 
 #################################
 ## (1) all markers
@@ -60,7 +65,7 @@ relevant_features <- all.markers
 output_filename <- paste0(output_filepath, "processed_", subset_number, "-cells_", features_included, "_")
 
 data.input <- dat.clean %>%
-  dplyr::select(all_of(relevant_features))
+  dplyr::select("gate", all_of(relevant_features))
 ##########
 ## MAIN ##
 ##########
@@ -86,7 +91,7 @@ output_filename <- paste0(output_filepath, "processed_", subset_number, "-cells_
 ## data
 
 data.input <- dat.clean %>%
-  dplyr::select(all_of(relevant_features))
+  dplyr::select("gate", all_of(relevant_features))
 
 
 ##########
@@ -100,7 +105,7 @@ phate_function(data.input) ## reticulate::use_python("/Users/mamouzgar/opt/anaco
 
 
 #################################
-## (2) scatterbodies
+## (3) scatterbodies
 #################################
 ## analysis-specific variables ##
 #################################
@@ -112,7 +117,7 @@ output_filename <- paste0(output_filepath, "processed_", subset_number, "-cells_
 ## data
 
 data.input <- dat.clean %>%
-  dplyr::select(all_of(relevant_features))
+  dplyr::select("gate", all_of(relevant_features))
 
 
 ##########
