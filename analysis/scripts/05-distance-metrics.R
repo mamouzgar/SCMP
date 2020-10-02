5f11c5e1c441d6af518e819b886992f1f7b001d3
Meelad Amouzgar <mamouzgar@DNa80e92e.SUNet>
2020-10-01 23:08:21 -0700

    fixed mahalanbis distance function to be faster:

--- analysis/scripts/05-distance-metrics.R
+++ analysis/scripts/05-distance-metrics.R
@@ -1,6 +1,7 @@
 
 library(gridExtra)
 library(ggpubr)
+library(tidyverse)
 setwd("/Users/mamouzgar/phd-projects")
 
 #### function to calculate the distance from centroid for files outputted from the function "extract_plot_columns" in 03-cluster-analysis script.
@@ -43,9 +44,9 @@ calculate_mahalanobis_dist <- function(filepath) {
   data.input <- data.table::fread(filepath)
   centroids <- data.input %>% group_by(labels) %>% summarize(x.centroid = mean(axis1), y.centroid = mean(axis2))
   label.combinations <- data.frame(tidyr::crossing(points = centroids$labels, centroid = centroids$labels), stringsAsFactors = FALSE) ## X1 is the point, CENTROID, and X2 are the cells
-
-  mahalanobis_outputs <- apply(label.combinations , MARGIN = 1, function(label.combination) { 
-    class.labels = c(label.combination[[1]],label.combination[[2]]) ## 1 =  class label for POINTS, 2 = class label for CENTROID
+  
+  mahalanobis_outputs <- lapply(1:nrow(label.combinations) , function(label.combination) { 
+    class.labels = c(label.combinations[label.combination,1],label.combinations[label.combination, 2]) ## 1 =  class label for POINTS, 2 = class label for CENTROID
     
     data.output <- data.input %>% dplyr::filter(., labels %in% class.labels[1]) %>%
       mutate(mahalanobis_dist  = mahalanobis(x = dplyr::select(., axis1, axis2),
@@ -54,18 +55,23 @@ calculate_mahalanobis_dist <- function(filepath) {
              ## mahalanobis function outputs SQUARED MAHALANOBIS DISTANCE,
              ## take the sqrt of mahalanovis output
              mahalanobis_dist = sqrt(mahalanobis_dist),
-             centroid.identity = class.labels[2]) %>%
-      spread(key = "centroid.identity", value = "mahalanobis_dist") 
+             centroid.identity = class.labels[2]) 
+    # %>%
+    # spread(key = "centroid.identity", value = "mahalanobis_dist")
     
     return(data.output)
   })
-  
-  mahalanobis_summary <- plyr::join_all(mahalanobis_outputs, by = "cell.id", type = "full")
-  
-  
+  mahalanobis_summary <- do.call("bind_rows",mahalanobis_outputs ) %>%
+    spread(key = "centroid.identity", value = "mahalanobis_dist")
+
   ## filename is a global variable
   write.table(mahalanobis_summary, paste0("SCMP/data/summary-tables/mahalanobis-distance/mahalanobis-dist_", basename(filepath)), sep = ",", row.names = FALSE, col.names = TRUE)
+  return(mahalanobis_summary)
 }
+
+ 
+
+
 
 
 ##########
@@ -78,7 +84,7 @@ analysis.ready.tables <- list.files("SCMP/data/analysis-ready/unsupervised-analy
 dist_from_centroid <- lapply(analysis.ready.tables, function(filepath) { 
   filepath <<- filepath
   print(filepath)
-  summary.table <- calculate_dist_from_centroid(filepath, writeTable = TRUE) 
+  summary.table <- calculate_dist_from_centroid(filepath, writeTable = FALSE) 
   return(summary.table)
 }) 
 dist_from_centroid <- do.call("bind_rows", dist_from_centroid)
@@ -108,7 +114,7 @@ summary_dist_from_centroid <- summary_dist_from_centroid %>%
          method = gsub(".*_|.csv","",filepath),
          data_balance = gsub(".*scatterbodies_|_.*","", filepath)) %>%
   arrange(data_balance, cell.count, method,labels)
-  
+
 
 write.table(summary_dist_from_centroid, "SCMP/data/summary-tables/dist-from-centroid_summary-analysis.csv",sep=",",row.names = F,col.names = T)
 
@@ -135,7 +141,7 @@ line_plot <- function(data , x , y, group, color, method, data.filter) {
              x=x, y=y, group=group, color=color,
              facet.by=method, nrow=1, title = data.filter) +
     rotate_x_text(90)
-
+  
   # p <- ggplot(filter(centroid.dists, data_balance == data.filter),
   #             aes(x=x, y=y, group=group, color=color)) + 
   #   geom_line() +
@@ -170,32 +176,65 @@ dev.off()
 # print(p)
 
 
+#################
+## mahalanobis ##
+#################
+mahalanobis.tables <- list.files("SCMP/data/summary-tables/mahalanobis-distance", full.names = TRUE)
 
+### get table of all cells for distances from centroid
+mahalanobis_dist <- lapply(mahalanobis.tables, function(filepath) { 
+  filepath <<- filepath
+  print(filepath)
+  summary.table <- data.table::fread(filepath) %>% mutate(filepath=basename(filepath))
+  return(summary.table)
+}) 
+mahalanobis_dist <- do.call("bind_rows", mahalanobis_dist)
+
+mahalanobis_dist <- mahalanobis_dist %>%
+  mutate(cell.count = gsub(".*processed_|-cells.*","", filepath),
+         method = gsub(".*_|.csv","",filepath),
+         data_balance = gsub(".*scatterbodies_|_.*","", filepath)) %>%
+  arrange(data_balance, cell.count, method,labels)
+write.table(mahalanobis_dist, "SCMP/data/summary-tables/mahalanobis-dist_full-analysis.csv",sep=",",row.names = F,col.names = T)
+
+
+## plots
+mahalanobis_dist <- data.table::fread("SCMP/data/summary-tables/mahalanobis-dist_full-analysis.csv")
+mahalanobis_dist_plot <- mahalanobis_dist %>%
+  dplyr::select(-filepath) %>%
+  gather(key = "label.centroid" , value = "mahalanobis_dist", -axis1, -axis2, -cell.id, -labels, -cell.count,-method,-data_balance)
+
+ggscatter(dplyr::filter(mahalanobis_dist_plot, cell.count == 548, method == "lda", data_balance == "balanced"),
+          x = "axis1", y = "axis2", 
+          color = "mahalanobis_dist",
+          facet.by = c("labels", "label.centroid")
+)
 
 ## all aggregated centroid data (very large data)
-centroid.dists <- data.table::fread("SCMP/data/summary-tables/dist-from-centroid_full-analysis.csv")
-ggboxplot(filter(centroid.dists, data_balance == "balanced"), 
-          x = "cell.count", y = "abs.dist.from.centroid", 
-          color = "labels",
-          yscale = "log2",scales = "free_x",
-          facet.by = c("method", "labels"), nrow = 2  )
-
-ggboxplot(filter(centroid.dists, data_balance == "unbalanced"), 
-         x = "cell.count", y = "abs.dist.from.centroid", 
-         add = "median",
-         color = "labels",
-         # yscale = "log2",
-         scales = "free_x",
-         facet.by = c("method", "labels"), nrow = 2  )
-
-p<- ggplot(filter(centroid.dists, data_balance == "balanced"), aes(x=cell.count, y=median.dist.from.centroid, group=labels, color=labels)) + 
-  geom_line() +
-  geom_point() +
-  # geom_errorbar(aes(ymin=median.dist.from.centroid-sd.dist.from.centroid, ymax=median.dist.from.centroid+sd.dist.from.centroid), width=.2,
-  #               position=position_dodge(0.05)) +
-  facet_wrap(~method , nrow = 1, scales = "free_x") +
-  theme_pubr()
-print(p)
+##### too much data, difficult to plot
+# centroid.dists <- data.table::fread("SCMP/data/summary-tables/dist-from-centroid_full-analysis.csv")
+# ggboxplot(filter(centroid.dists, data_balance == "balanced"), 
+#           x = "cell.count", y = "abs.dist.from.centroid", 
+#           color = "labels",
+#           yscale = "log2",scales = "free_x",
+#           facet.by = c("method", "labels"), nrow = 2  )
+# 
+# ggboxplot(filter(centroid.dists, data_balance == "unbalanced"), 
+#          x = "cell.count", y = "abs.dist.from.centroid", 
+#          add = "median",
+#          color = "labels",
+#          # yscale = "log2",
+#          scales = "free_x",
+#          facet.by = c("method", "labels"), nrow = 2  )
+# 
+# p<- ggplot(filter(centroid.dists, data_balance == "balanced"), aes(x=cell.count, y=median.dist.from.centroid, group=labels, color=labels)) + 
+#   geom_line() +
+#   geom_point() +
+#   # geom_errorbar(aes(ymin=median.dist.from.centroid-sd.dist.from.centroid, ymax=median.dist.from.centroid+sd.dist.from.centroid), width=.2,
+#   #               position=position_dodge(0.05)) +
+#   facet_wrap(~method , nrow = 1, scales = "free_x") +
+#   theme_pubr()
+# print(p)
 
 
 
